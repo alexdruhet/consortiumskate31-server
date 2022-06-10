e45c6e78a496af374a2e576f98babf96d5b27279
Alex Druhet <alex@listo.studio>
2022-06-10 16:35:36 +0200

    feat: scaffold post submission skeleton

--- server.ts
+++ server.ts
@@ -1,7 +1,31 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 
-serve((_req) => {
-    return new Response("Hello World!", {
-      headers: { "content-type": "text/plain" },
-    });
-  });
+async function handler(req: Request): Promise<Response> {
+    switch (req.method) {
+        case "POST": {
+            const body = await req.formData();
+            const name = body.get("name") || "anonymous";
+            const message = body.get("message");
+            //return new Response(`Hello ${name}!`);
+
+            const data = {
+                message: "sent!",
+            };
+
+            const response = JSON.stringify(data, null, 2);
+
+            return new Response(response, {
+                headers: { "content-type": "application/json; charset=utf-8" },
+            });
+        }
+
+        default:
+            return new Response("Invalid method", {
+                status: 405,
+                headers: { "content-type": "application/json; charset=utf-8" },
+            });
+    }
+}
+
+serve(handler);
+
