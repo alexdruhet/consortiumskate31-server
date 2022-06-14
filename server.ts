71ca5ec4f8e3a681c6a90e3a2e303bd8bb2f9699
Alex Druhet <alex@listo.studio>
2022-06-14 17:36:32 +0200

    build: functional beta version

--- server.ts
+++ server.ts
@@ -1,26 +1,30 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 import "https://deno.land/x/dotenv/load.ts";
-import { sendMail, IRequestBody } from "https://deno.land/x/sendgrid/mod.ts";
+import { IRequestBody, sendMail } from "https://deno.land/x/sendgrid/mod.ts";
 import { email } from "https://deno.land/x/validation/mod.ts";
 
-
 async function handler(req: Request): Promise<Response> {
-    const { ALLOWED_ORIGIN, EMAIL_TO, NAME_TO, SENDGRID_API_KEY } = Deno.env.toObject();
+    const {
+        ALLOWED_ORIGIN,
+        EMAIL_TO,
+        NAME_TO,
+        SENDGRID_API_KEY
+    } = Deno.env.toObject();
     const responseHeaders = {
         "Content-Type": "application/json; charset=utf-8",
         "Access-Control-Allow-Origin": `${ALLOWED_ORIGIN}`,
-        "Vary": "Origin"
+        "Vary": "Origin",
     };
 
     if (
-        !req.headers.has("origin")
-        || (
-            ALLOWED_ORIGIN !== "*"
-            && req.headers.get("origin") !== ALLOWED_ORIGIN
+        !req.headers.has("origin") ||
+        (
+            ALLOWED_ORIGIN !== "*" &&
+            req.headers.get("origin") !== ALLOWED_ORIGIN
         )
     ) {
         const data = {
-            message: "Forbidden access"
+            message: "Forbidden access",
         };
         return new Response(JSON.stringify(data, null, 2), {
             status: 403,
@@ -31,38 +35,39 @@ async function handler(req: Request): Promise<Response> {
     switch (req.method) {
         case "POST": {
             let hasError: boolean = false;
-            let errors = { 'email': '', 'name': '', 'message': '' };
+            let errors = { "email": "", "name": "", "message": "" };
 
             const body: FormData = await req.formData();
 
-            const emailFrom: FormDataEntryValue | string = body.get("email") || '';
-            console.log('Email: ', emailFrom);
+            const emailFrom: FormDataEntryValue | string = body.get("email_from") || "";
             if (!email.valid(emailFrom.toString())) {
                 hasError = true;
-                errors.email = 'invalid email';
+                errors.email = "invalid email";
                 console.error(errors.email);
             }
 
-            const subject: string = `[Web Contact] message de ${emailFrom}`
+            const subject: string = `[Web Contact] message de ${emailFrom}`;
 
-            const name: FormDataEntryValue | string = body.get("name") || '';
-            console.log('Name: ', name);
+            const name: FormDataEntryValue | string = body.get("name") || "";
             if (!name.toString()) {
                 hasError = true;
-                errors.name = 'empty name';
+                errors.name = "empty name";
                 console.error(errors.name);
             }
 
-            const message: FormDataEntryValue | string = body.get("message") || '';
-            console.log('Message: ', message);
+            const message: FormDataEntryValue | string = body.get("message") || "";
             if (!message.toString()) {
                 hasError = true;
-                errors.message = 'empty message';
+                errors.message = "empty message";
                 console.error(errors.message);
             }
 
             if (hasError) {
-                const response = JSON.stringify({ 'message': 'invalid data', 'errors': errors }, null, 2);
+                const response = JSON.stringify(
+                    { "message": "invalid data", "errors": errors },
+                    null,
+                    2,
+                );
 
                 return new Response(response, {
                     status: 400,
@@ -77,7 +82,7 @@ async function handler(req: Request): Promise<Response> {
                         to: [{ name: NAME_TO, email: EMAIL_TO }],
                     },
                 ],
-                from: { name: `${name.toString()}`, email: `${emailFrom.toString()}` },
+                from: { name: NAME_TO, email: EMAIL_TO },
                 content: [
                     { type: "text/plain", value: `${message.toString()}` },
                 ],
@@ -85,10 +90,24 @@ async function handler(req: Request): Promise<Response> {
 
             let sendGridResponse = await sendMail(mail, { apiKey: SENDGRID_API_KEY });
 
-            console.log(sendGridResponse);
+            if (sendGridResponse.success === false) {
+                console.error(sendGridResponse);
+                const response = JSON.stringify(
+                    { "message": "Error while sending message", "errors": sendGridResponse.errors },
+                    null,
+                    2,
+                );
+
+                return new Response(response, {
+                    status: 400,
+                    headers: responseHeaders,
+                });
+            }
+
+            console.info(`${subject} successfully sent`);
 
             const data = {
-                message: "sent!"
+                message: "sent!",
             };
 
             const response = JSON.stringify(data, null, 2);
@@ -100,7 +119,7 @@ async function handler(req: Request): Promise<Response> {
 
         default:
             const data = {
-                message: "Invalid method"
+                message: "Invalid method",
             };
             return new Response(JSON.stringify(data, null, 2), {
                 status: 405,
