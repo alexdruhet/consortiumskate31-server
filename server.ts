ee18e991e5f7a28443da655d07b360c2fcd2c46a
Alex Druhet <alex@listo.studio>
2022-06-14 15:46:28 +0200

    fix: wip: use sendgrid

--- server.ts
+++ server.ts
@@ -1,23 +1,24 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
-import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
+import { sendMail, IRequestBody } from "https://deno.land/x/sendgrid/mod.ts";
+import { email } from "https://deno.land/x/validation/mod.ts";
 
 
 async function handler(req: Request): Promise<Response> {
-    const { ALLOWED_ORIGIN, EMAIL_TO, PASSWORD } = Deno.env.toObject();
+    const { ALLOWED_ORIGIN, EMAIL_TO, NAME_TO, SENDGRID_API_KEY } = Deno.env.toObject();
     const responseHeaders = {
         "Content-Type": "application/json; charset=utf-8",
         "Access-Control-Allow-Origin": `${ALLOWED_ORIGIN}`,
         "Vary": "Origin"
     };
 
-    console.log(req.headers.get('origin'), ALLOWED_ORIGIN, req.headers);
+    console.log(req.headers.get("origin"), ALLOWED_ORIGIN, req.headers);
 
     if (
-        !req.headers.has('origin')
+        !req.headers.has("origin")
         || (
-            ALLOWED_ORIGIN !== '*'
-            && req.headers.get('origin') !== ALLOWED_ORIGIN
+            ALLOWED_ORIGIN !== "*"
+            && req.headers.get("origin") !== ALLOWED_ORIGIN
         )
     ) {
         const data = {
@@ -31,28 +32,52 @@ async function handler(req: Request): Promise<Response> {
 
     switch (req.method) {
         case "POST": {
+            let hasError = false;
+            let errors = [];
             const body = await req.formData();
-            const emailFrom = body.get("email") || EMAIL_TO;
-            const name = body.get("name") || "anonymous";
-            const message = body.get("message") || "test";
+            const emailFrom = body.get("email");
+            if (!email.valid(emailFrom)) {
+                hasError = true;
+                errors['email'] = 'invalid email';
+                console.error(errors['email']);
+            }
+            const subject = `[Web Contact] message de ${emailFrom}`
+            const name = body.get("name");
+            if (!name) {
+                hasError = true;
+                errors['name'] = 'empty name';
+                console.error(errors['name']);
+            }
+            const message = body.get("message");
+            if (!message) {
+                hasError = true;
+                errors['message'] = 'empty message';
+                console.error(errors['message']);
+            }
 
-            const client = new SmtpClient();
-            const connectConfig: any = {
-                hostname: "smtp.gmail.com",
-                port: 465,
-                username: EMAIL_TO,
-                password: PASSWORD,
+            if (hasError) {
+                const response = JSON.stringify(errors, null, 2);
+
+                return new Response(response, {
+                    status: 400,
+                    headers: responseHeaders,
+                });
+            }
+
+            let mail: IRequestBody = {
+                personalizations: [
+                    {
+                        subject: `${subject}`,
+                        to: [{ name: NAME_TO, email: EMAIL_TO }],
+                    },
+                ],
+                from: { name: `${name}`, email: `${emailFrom}` },
+                content: [
+                    { type: "text/plain", value: `${message}` },
+                ],
             };
-            //await client.connectTLS(connectConfig);
 
-            //await client.send({
-            //    from: emailFrom,
-            //    to: EMAIL_TO,
-            //    subject: `[${ALLOWED_ORIGIN}] message de ${name}`,
-            //    content: message,
-            //});
-
-            //await client.close();
+            let sendGridResponse = await sendMail(mail, { apiKey: SENDGRID_API_KEY });
 
             const data = {
                 message: "sent!"
